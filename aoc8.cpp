92aff2baea2dc825464c65ba6a1963fbb18476ab
s4naaa <s8430801@gmail.com>
2020-12-08 00:14:52 -0600

    8th day

---
+++ aoc8.cpp
@@ -0,0 +1,118 @@
+
+#include "dataloader.hpp"
+#include <unordered_map>
+
+using ll = long long;
+
+struct InstructionData {
+  std::string command;
+  ll val;
+  bool ran;
+};
+
+class Interpreter {
+public:
+  Interpreter(const std::string &path) : pos(0) {
+    const auto data = load_data(path);
+
+    const auto get_instruction = [](const std::string &line) {
+      return std::string(line.begin(), line.begin() + 3);
+    };
+
+    const auto get_value = [](const std::string &line) -> ll {
+      return static_cast<ll>(std::stoi(std::string(line.begin() + 4, line.end())));
+    };
+
+    for (const auto &line : data)
+      instruction_info.push_back({get_instruction(line), get_value(line), false});
+  }
+  
+  void read_line(void) {
+    auto &instruction = instruction_info[pos];
+    
+    if (instruction.ran || 
+       (pos >= instruction_info.size() && 
+        !(instruction.command == "jmp" &&
+          instruction.val < 0)))
+      terminated = true;
+    else {
+      instruction.ran = true;
+      instructions.find(instruction.command)->second(instruction.val);
+    }
+  }
+
+  void run(void) {
+    pos = 0;
+    terminated = false;
+    val = 0;
+
+    for (auto &instruction : instruction_info)
+      instruction.ran = false;
+
+    while (!terminated)
+      read_line();
+  }
+
+  inline const ll &get_val(void) const { return val; }
+
+  inline std::size_t get_size(void) const { return instruction_info.size(); }
+
+  inline const std::size_t &get_pos(void) const { return pos; }
+
+  inline InstructionData &get_instruction(const std::size_t i) { 
+    return instruction_info[i];
+  }
+
+  std::vector<InstructionData> instruction_info;
+
+private:
+  std::unordered_map<std::string, std::function<void(const ll)>> instructions = {
+    {"acc", [&](const ll num) {
+      val += num;
+      ++pos;
+    }},
+    {"jmp", [&](const ll num) {
+      pos = static_cast<std::size_t>(pos + num);
+    }},
+    {"nop", [&](const ll num) {
+      ++pos;
+    }}
+  };
+
+  std::size_t pos;
+  ll val;
+  bool terminated;
+};
+
+
+
+int main(void) {
+
+  auto a = Interpreter("input8.txt");
+  const std::size_t s = a.get_size(); // just to be careful, im going to manage
+
+  for (std::size_t i = 0; i < s; ++i) {
+    auto &instruction = a.get_instruction(i);
+
+    if (instruction.command == "nop") {
+      instruction.command = "jmp";
+      a.run();
+      if (a.get_size() == a.get_pos()) {
+        std::cout << a.get_val() << '\n';
+        return 0;
+      }
+      instruction.command = "nop";
+    } else if (instruction.command == "jmp") {
+      instruction.command = "nop";
+      a.run();
+      if (a.get_size() == a.get_pos()) {
+        std::cout << a.get_val() << '\n';
+        return 0;
+      }
+      instruction.command = "jmp";
+    }
+  }
+
+  return 0;
+}
+
